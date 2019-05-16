0364cc49a3f94e05c4b0a7a62c82f7e0c021790a
gatta <trabajogatta@hotmail.com>
2019-05-16 13:31:18 +0200

    cambioo

---
+++ dados.js
@@ -0,0 +1,15 @@
+class Dado {
+    constructor (face=Number){
+        this.face = face;
+    }
+    function (jugar_){
+        let dado_1 = 16;
+        let dado_2 = 7;
+        if (this.face =dado_1 / 2 *dado_1 *100);
+        return this.face;
+    }
+    function (pares){
+        
+    }
+}
+console.log("asdfhjioadsf")
